c9a399dc7379d2af5e2e2a9dbf3fc141b67edf36
Igor Leal <igorhenriqueleal3@gmail.com>
2026-04-27 22:51:51 +0000

    Criadas as variáveis das cartas 1 e 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,6 +6,27 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  // Carta 1
+  int carta1;
+  char estado1;
+  char codigo1[10];
+  char cidade1[50];
+  int populacao1;
+  float area1;
+  float pib1;
+  int pontosTuristicos1;
+
+  // Carta 2
+  int carta2;
+  char estado2;
+  char codigo2[10];
+  char cidade2[50];
+  int populacao2;
+  float area2;
+  float pib2;
+  int pontosTuristicos2;
+
+
 
   // Área para entrada de dados
 
